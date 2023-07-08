ff26758db3abc2631a10a896a4ec176ab29c02e7
mark279455 <mark279455@ntlworld.com>
2023-07-08 18:03:42 +0100

    index.html - pics uploaded - working on html / css

--- home/views.py
+++ home/views.py
@@ -1,16 +1,6 @@
 from django.views.generic import TemplateView
-import os
-import random
-import glob
 
 
 class Index(TemplateView):
     template_name = 'home/index.html'
 
-
-def randomhomepageimage(dirname):
-    valid_fileanme = "restaurant"
-    valid_extension = '.webp'
-
-    files = glob.glob(dirname+'/'+valid_fileanme+'?'+valid_extension)
-    return dirname + "/" + random.choice(files)
